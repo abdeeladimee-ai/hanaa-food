63618c9f2b2da80c6d1629cea76cb38046d6ad85
abdeeladimee-ai <abdeeladimee@gmail.com>
2026-09-13 22:30:42 +0100

    Enable mandatory customer location gate

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './customerLocationGate.js'
 import './qzAutoPrint.js'
 import './kitchenAutoPrint.js'
 import App from './App.jsx'
