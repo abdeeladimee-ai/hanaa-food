f8cbd9d589be1ca54709f86d585982413e9cdf9a
abdeeladimee-ai <abdeeladimee@gmail.com>
2026-09-21 02:30:55 +0100

    Restore stable version of src/pages/DriverDashboard.jsx

--- src/pages/DriverDashboard.jsx
+++ src/pages/DriverDashboard.jsx
@@ -1,17 +1,5 @@
-import { lazy, Suspense } from "react";
-
-const RoleWorkflow = lazy(() => import("../RoleWorkflow"));
+import RoleWorkflow from "../RoleWorkflow";
 
 export default function DriverDashboard({ onHome, session }) {
-  return (
-    <Suspense fallback={<main className="simple-page narrow">Chargement...</main>}>
-      <RoleWorkflow
-        role="driver"
-        session={session}
-        orderType="delivery"
-        title="Livraisons"
-        onHome={onHome}
-      />
-    </Suspense>
-  );
+  return <RoleWorkflow role="driver" session={session} orderType="delivery" title="Livraisons" onHome={onHome} />;
 }
