9f1cee2b3dfe382a5b691d106f3d9c658d38446d
abdeeladimee-ai <abdeeladimee@gmail.com>
2026-09-24 16:41:45 +0100

    Pause customer orders until reopening (#20)
    * ops: pause customer ordering in client
    
    * ops: pause customer ordering on server

--- api/neon-orders.js
+++ api/neon-orders.js
@@ -5,6 +5,7 @@ import { verifiedStaffFromRequest } from "../lib/staffAuth.js";
 const MAX_LIMIT = 200;
 const MAX_ORDER_BYTES = 64 * 1024;
 const MAX_ACTIVE_ORDERS_PER_PHONE = 2;
+const CUSTOMER_ORDERING_PAUSED = true;
 const IP_WINDOW_MINUTES = 10;
 const IP_MAX_ATTEMPTS = 10;
 const IP_BLOCK_MINUTES = 15;
@@ -192,7 +193,7 @@ async function createOrder(req, res) {
     return json(res, 413, { ok: false, code: "ORDER_TOO_LARGE" });
   }
 
-  if (await orderingPaused(getPool())) {
+  if (CUSTOMER_ORDERING_PAUSED || await orderingPaused(getPool())) {
     return json(res, 423, { ok: false, code: "CUSTOMER_ORDERING_PAUSED" });
   }
 
